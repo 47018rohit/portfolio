8db3fbb589b8047d7898f7527b9b7240c5cf1e74
rohit prakash <rohit.p@celcius.in>
2026-02-07 13:52:11 +0530

    new clean

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import PortfolioMain from '@/Pages/PortfolioMain'
-
-
-export const Route = createFileRoute('/')({ component: Portfolio })
-
-
-function Portfolio() { return <PortfolioMain /> }
--- src/routes/test/- test.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import TestPage from '@/Pages/Test'
-
-export const TestRoute = createFileRoute('/')({ component: Test })
-
-
-function Test() { return <TestPage /> }
